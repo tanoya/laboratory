87fb8411cb9064fa0e40afb704ed4079ec5339e2
tanxiangya@corp.netease.com <tanxiangya@xiaomi.com>
2021-07-08 20:20:18 +0800

    添加查找的性能测试

--- reg/java/test/src/main/java/com/toy/application/Application.java
+++ reg/java/test/src/main/java/com/toy/application/Application.java
@@ -4,19 +4,44 @@ package com.toy.application;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
 
     static String imgReg = "<p[^<>]*>[^<>]*<img[^<>]*\\ssrc=(?:'|\")?(?:[^\\s'\"<>]+)//s\\.cimg\\.163\\.com/i/([^\\s'\"<>]+?)\\.\\d+x\\d+\\.auto.jpg(?:'|\")?[^<>]*>(.*?)</p>";
     static String tagReg = "<[^<>]*>";
     static String whiteSpaceReg = "[\\s　]*";
+
+    static Pattern imgRegPattern = Pattern.compile(imgReg);
+    static Pattern tagRegPattern = Pattern.compile(tagReg);
+    static Pattern whiteSpaceRegPattern = Pattern.compile(whiteSpaceReg);
+
     static int threshold = 10000;
     public static void main(String[] args) {
         run();
+        find();
     }
 
     static String format(String app, String method, long costTime) {
         return String.format(" %s %s cost %d millionSecond. \n", app, method, costTime);
+    }
+
+    static void find() {
+        Long start = System.currentTimeMillis();
+        for(int i=1; i<threshold; i++){
+            find01();
+        }
+        Long end = System.currentTimeMillis();
+        System.out.print(format("java", "find", end-start));
+    }
+
+    static void find01() {
+        String data = read("src/main/resources/data/online_data.txt");
+        Matcher matcher = imgRegPattern.matcher(data);
+        while( matcher.find() ){
+
+        }
     }
 
     static void run() {
